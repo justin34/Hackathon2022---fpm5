532c79f5336c977aad43001566320f565757bece
Jabril Hall <jabril6187@gmail.com>
2022-06-25 18:07:22 -0400

    added image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 if __name__ == '__main__':
     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-    im = Image.open(r'C:\Users\Jabri\Downloads\image.png')
+    im = Image.open(r'image.png')
     text = pytesseract.image_to_string(im, lang='ara')
     print(text)
 
